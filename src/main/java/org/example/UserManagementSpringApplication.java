5ca0fe8289e17f475081e0facee1d5e58b816fee
witzealanup <125854559+witzealanup@users.noreply.github.com>
2025-06-25 23:45:01 +0530

    testing

--- src/main/java/org/example/UserManagementSpringApplication.java
+++ src/main/java/org/example/UserManagementSpringApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserManagementSpringApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(UserManagementSpringApplication.class,args);
+        
     }
 }
